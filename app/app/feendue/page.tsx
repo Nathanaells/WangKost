dda87dbb4acd647fb6dfea4396a82bb34bd29028
RaliefRivansyah <ralief.rivansyah@gmail.com>
2025-12-11 16:14:25 +0700

    feat: Sidebar

--- app/app/feendue/page.tsx
+++ app/app/feendue/page.tsx
@@ -1,11 +1,12 @@
-export default function Dashboard() {
+export default function feendues() {
     return (
         <div className="p-4 pt-20 sm:ml-64 bg-gray-50 min-h-screen">
             <div className="max-w-7xl mx-auto pt-6">
                 {/* Header */}
                 <div className="flex justify-between items-start mb-8">
                     <div>
-                        <h1 className="text-2xl font-bold text-gray-900">Dashboard</h1>
+                        <h1 className="text-2xl font-bold text-gray-900">Fee and Due</h1>
+                        <p className="text-gray-500 mt-1">Manage fee and dues </p>
                     </div>
                 </div>
             </div>
