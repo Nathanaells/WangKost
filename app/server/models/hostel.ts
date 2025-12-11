968621e695e1668370f820a5489c0376c9fdd788
HozFisen <hozfisen@gmail.com>
2025-12-11 13:52:24 +0700

    feat: admin api test

--- app/server/models/hostel.ts
+++ app/server/models/hostel.ts
@@ -1,27 +1,22 @@
-import { ObjectId, Collection, WithId, Document } from "mongodb";
-import { getDb } from "../mongodb/config";
+import { Model, IMongoloquentSchema, IMongoloquentTimestamps } from "mongoloquent";
 
-export interface IHostel {
-    _id?: ObjectId;
-    name: string;
-    address: string;
-    description: string;
-    maxRooms: number;
-    adminId: ObjectId;
-    createdAt?: Date;
-    updatedAt?: Date;
+interface IHostel extends IMongoloquentSchema, IMongoloquentTimestamps {
+  name: string;
+  address: string;
+  maxRoom?: number;
+  description?: string;
+  adminId: string;
 }
 
-export default class Hostel {
-    static getCollection(): Collection<IHostel> {
-        const db = getDb();
-        const collection = db.collection<IHostel>("hostels");
-        return collection;
-    }
+class Hostel extends Model<IHostel> {
+  /**
+   * The attributes of the model.
+   *
+   * @var IHostel
+   */
+  public static $schema: IHostel
 
-    static async getHostels(): Promise<WithId<IHostel>[]> {
-        const collection = this.getCollection();
-        const hostels = await collection.find().toArray();
-        return hostels;
-    }
+  // ...
+  protected $collection: string = "hostels";
 }
+
